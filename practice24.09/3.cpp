0ceae92f258c9b0d98a5d725459b5176dff814c7
Pavel Mokeev <pav3l.mokeev@gmail.com>
2021-10-02 14:32:45 +0300

    added third task

--- practice24.09/3.cpp
+++ practice24.09/3.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-int set_i_bit_one(int current_x, int current_i) {
-    current_x |= 1 << current_i;
+int set_i_bit_zero(int current_x, int current_i) {
+    current_x &= ~(1UL << current_i);
 
     return current_x;
 }
 
-int main() { 
+int main() {
     int x, i;
     std::cout << "Enter x:" << std::endl;
     std::cin >> x;
     std::cout << "Enter i:" << std::endl;
     std::cin >> i;
 
-    x = set_i_bit_one(x, i);
+    x = set_i_bit_zero(x, i);
     std::cout << "Result = " << x << std::endl;
     return 0;
 }
