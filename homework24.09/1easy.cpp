911729e81ebdcb385bef2ae1004752076238a18c
Pavel Mokeev <pav3l.mokeev@gmail.com>
2021-10-02 15:25:37 +0300

    added 1st task(easy)

---
+++ homework24.09/1easy.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int switch_bit(int current_x, int current_i) {
+    int bit = (current_x & ( 1 << current_i )) >> current_i;
+
+    if (bit) {
+        current_x &= ~(1UL << current_i);
+    } else {
+        current_x |= 1 << current_i;
+    }
+
+    return current_x;
+}
+
+int main() {
+    int x, i;
+    std::cout << "Enter x and i:" << std::endl;
+    std::cin >> x >> i;
+    x = switch_bit(x, i);
+
+    std::cout << "Новый x = " << x << std::endl;
+
+    return 0;
+}
